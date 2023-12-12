f00d6489349c951ed59daf980794663bebc80ee1
Michael Koritschan <yaknowho@gmail.com>
2023-12-12 16:06:31 +0100

    streamlined ellipses-generation for avatar

--- js/p5mainEnvironment.js
+++ js/p5mainEnvironment.js
@@ -8,17 +8,26 @@ const baseSize = 250;
 function draw() {
     background(0);
     fill(255);
-    ellipse(width-nosePosition[0]*width, nosePosition[1]*height, nosePosition[2]*baseSize);
-    ellipse(width-leftHandPosition[0]*width, leftHandPosition[1]*height, leftHandPosition[2]*(baseSize/2));
-    ellipse(width-rightHandPosition[0]*width, rightHandPosition[1]*height, rightHandPosition[2]*(baseSize/2));
-    ellipse(width-leftElbowPosition[0]*width, leftElbowPosition[1]*height, leftElbowPosition[2]*(baseSize/3));
-    ellipse(width-rightElbowPosition[0]*width, rightElbowPosition[1]*height, rightElbowPosition[2]*(baseSize/3));
-    ellipse(width-leftShoulderPosition[0]*width, leftShoulderPosition[1]*height, leftShoulderPosition[2]*(baseSize/3));
-    ellipse(width-rightShoulderPosition[0]*width, rightShoulderPosition[1]*height, rightShoulderPosition[2]*(baseSize/3));
-    ellipse(width-leftHipPosition[0]*width, leftHipPosition[1]*height, leftHipPosition[2]*(baseSize/3));
-    ellipse(width-rightHipPosition[0]*width, rightHipPosition[1]*height, rightHipPosition[2]*(baseSize/3));
-    ellipse(width-leftKneePosition[0]*width, leftKneePosition[1]*height, leftKneePosition[2]*(baseSize/3));
-    ellipse(width-rightKneePosition[0]*width, rightKneePosition[1]*height, rightKneePosition[2]*(baseSize/3));
-    ellipse(width-leftToePosition[0]*width, leftToePosition[1]*height, leftToePosition[2]*(baseSize/2));
-    ellipse(width-rightToePosition[0]*width, rightToePosition[1]*height, rightToePosition[2]*(baseSize/2));
+    
+    const positions = [
+      nosePosition,
+      leftHandPosition,
+      rightHandPosition,
+      leftElbowPosition,
+      rightElbowPosition,
+      leftShoulderPosition,
+      rightShoulderPosition,
+      leftHipPosition,
+      rightHipPosition,
+      leftKneePosition,
+      rightKneePosition,
+      leftToePosition,
+      rightToePosition
+    ];
+  
+    positions.forEach(position => {
+      const [x, y, z] = position;
+      const size = baseSize / (position === nosePosition ? 1 : (position === leftHandPosition || position === rightHandPosition || position === leftToePosition || position === rightToePosition) ? 2 : 3);
+      ellipse(width - x * width, y * height, z * size);
+    });
   }
